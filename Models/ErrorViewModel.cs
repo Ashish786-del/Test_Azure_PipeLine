bb9790d507fc9c5b8685af69201f7b691a07de13
chourasiaa30 <chourasiaa30@gmail.com>
2020-02-15 16:19:29 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestAzurePipeLine.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
